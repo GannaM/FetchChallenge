9386e0c6ba379fd2f392c2ad1bbdcf7d2bd1e209
Anna Myshliakova <ganna.myshliakova@gmail.com>
2025-01-25 12:20:55 -0800

    Initial Commit

---
+++ FetchChallenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FetchChallenge
+//
+//  Created by Anna Myshliakova on 1/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FetchChallengeTests/FetchChallengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  FetchChallengeTests.swift
+//  FetchChallengeTests
+//
+//  Created by Anna Myshliakova on 1/25/25.
+//
+
+import Testing
+@testable import FetchChallenge
+
+struct FetchChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
